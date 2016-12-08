92821483ad3604194749b57fb9195e21a62b478b
Phuongpham09 <phuong.pham@nguonviet.vn>
2016-12-08 10:19:35 +0700

    ss

--- app/src/main/java/com/example/phuong/alarm/CaculatorActivity.java
+++ app/src/main/java/com/example/phuong/alarm/CaculatorActivity.java
@@ -21,6 +21,10 @@ import java.util.Random;
 
 public class CaculatorActivity extends AppCompatActivity {
     MediaPlayer mp;
+    private TextView tvNumber1;
+    private TextView tvNumber2;
+    private TextView tvNumber3;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,16 +33,13 @@ public class CaculatorActivity extends AppCompatActivity {
 
         requestWindowFeature(Window.FEATURE_NO_TITLE); //hide activity title
         setContentView(R.layout.dialog_caculator_alarm);
-        final Dialog dialog = new Dialog(this);
-        dialog.setTitle("Repeat");
-        dialog.setContentView(R.layout.dialog_caculator_alarm);
 
-        final TextView tvNumber1 = (TextView) dialog.findViewById(R.id.tvNumber1);
-        final TextView tvNumber2 = (TextView) dialog.findViewById(R.id.tvNumber2);
-        final TextView tvNumber3 = (TextView) dialog.findViewById(R.id.tvNumber3);
-        Button btnCancel = (Button) dialog.findViewById(R.id.btnCancel);
-        Button btnOk = (Button) dialog.findViewById(R.id.btnOk);
-        final EditText edtResult  = (EditText) dialog.findViewById(R.id.result);
+        final TextView tvNumber1 = (TextView) findViewById(R.id.tvNumber1);
+        final TextView tvNumber2 = (TextView) findViewById(R.id.tvNumber2);
+        final TextView tvNumber3 = (TextView) findViewById(R.id.tvNumber3);
+        Button btnCancel = (Button) findViewById(R.id.btnCancel);
+        Button btnOk = (Button) findViewById(R.id.btnOk);
+        final EditText edtResult  = (EditText) findViewById(R.id.result);
 
         Random rd = new Random();
         int number1 = rd.nextInt(100);
@@ -50,13 +51,6 @@ public class CaculatorActivity extends AppCompatActivity {
         tvNumber2.setText(String.valueOf(number2));
         tvNumber3.setText(String.valueOf(number3));
 
-        btnCancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dialog.dismiss();
-            }
-        });
-
         btnOk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -64,12 +58,14 @@ public class CaculatorActivity extends AppCompatActivity {
                 Log.d("TAG111",result+"");
                 if(String.valueOf(result).equals(edtResult.getText().toString())){
                     mp.release();
+                    onBackPressed();
                 }
                 else{
                     Toast.makeText(getApplication(),"Fail",Toast.LENGTH_SHORT).show();
                 }
             }
         });
+
 
     }
 }
